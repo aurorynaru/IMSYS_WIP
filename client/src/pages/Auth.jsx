f77a647b2df3244b1b92c3e11cea588c80e19264
aurorynaru <antonramos10@gmail.com>
2023-06-11 15:19:55 +0800

    auth

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div className='w-full bg-base-100 '>
+        <div className="w-1/2 bg-base-100"></div>
+        <div className="w-1/2 bg-base-100"></div>
+    </div>
+  )
+}
+
+export default Auth
